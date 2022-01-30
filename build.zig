5f4effa7c7f6c6eecf61ce59bfa18c4b6a472ccd
Alex G Rice <rice.guido@gmail.com>
2022-01-30 16:11:35 -0700

    Add zig library (stub) and improve comments.

--- build.zig
+++ build.zig
@@ -1,3 +1,4 @@
+/// this build.zig shows how to link libgeos with an exe and with your unit tests.
 const std = @import("std");
 const libgeos = @import("src/libgeos.zig");
 
@@ -5,31 +6,30 @@ pub fn build(b: *std.build.Builder) !void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
 
+    // the C api depends on the core C++ lib, so build and link both of them.
     const core_lib = try libgeos.createCore(b, target, mode);
-    core_lib.step.install();
     const capi_lib = try libgeos.createCAPI(b, target, mode);
-    capi_lib.step.install();
 
+    // setup unit tests step
     const tests = b.addTest("src/tests.zig");
     core_lib.link(tests, .{});
-    capi_lib.link(tests, .{});
+    capi_lib.link(tests, .{ .import_name = "geos" });
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&tests.step);
 
+    // setup run-example1 step
     const exe = b.addExecutable("example1", "src/examples/example1.zig");
     exe.addPackagePath("default_handlers", "src/shim/default_handlers.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
     core_lib.link(exe, .{});
-    capi_lib.link(exe, .{});
+    capi_lib.link(exe, .{ .import_name = "geos" });
     exe.install();
-
     const run_cmd = exe.run();
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
-
     const run_step = b.step("run-example1", "Run the example1 app");
     run_step.dependOn(&run_cmd.step);
 }
--- src/shim/zig_handlers.h
+++ src/shim/zig_handlers.h
@@ -1,2 +1,14 @@
+/*
+* GEOS uses varargs functions for it's notice handler and error handler.
+* Zig does not support varargs functions, so we have to use C functions
+* which then call back into Zig. So these shim functions accept variable args,
+* format them, and call back into Zig with the formatted results.
+*/
+
+// Format the notice string, then call noticeHandler().
+// noticeHandler owns the returned memory.
 void shimNotice(const char *fmt, ...);
+
+/// Format the error string, then call errorHandler.
+/// errorHandler owns the returned memory.
 void shimError(const char *fmt, ...);
