61cbfb8be21bb941e9cc52549b967d86a679d147
Alex G Rice <alex@ricegeo.dev>
2022-03-01 09:58:37 -0700

    Add failing unit test for c++  runtime_error. (#10)

--- src/tests.zig
+++ src/tests.zig
@@ -30,3 +30,23 @@ test "init/finish" {
     c.initGEOS(handlers.shimNotice, handlers.shimError);
     c.finishGEOS();
 }
+
+test "c++ error handler" {
+    // Send notice and error messages to our stdout handler
+    c.initGEOS(handlers.shimNotice, handlers.shimError);
+    // Clean up the global context
+    defer c.finishGEOS();
+    errdefer c.finishGEOS();
+
+    // badly formatted WKT poly
+    const wkt = "BADPOLYGON ((189 115, 200 170, 130 170, 35 242, 156 215, 210 290, 274 256, 360 190, 267 215, 300 50, 200 60, 189 115))";
+
+    // Read the WKT into geometry objects
+    const reader = c.GEOSWKTReader_create();
+    defer c.GEOSWKTReader_destroy(reader);
+
+    const geom = c.GEOSWKTReader_read(reader, wkt);
+    // TODO: the _read call crashes with: `libc++abi: terminating with uncaught exception of type geos::io::ParseException: ParseException: Unknown type: 'BADPOLYGON'`
+    defer c.GEOSGeom_destroy(geom);
+    try testing.expect(reader != null);
+}
